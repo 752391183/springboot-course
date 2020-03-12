b735f3f602a855655c29f5b40b2692945745ac6b
Administrator <s.y.zhang@nefu.edn.cn>
2020-03-12 11:22:36 +0800

    springboot-course项目的初始化

--- springboot-jpa/src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -1,9 +1,14 @@
 package com.example.springbootjpa;
 
+import com.example.springbootjpa.repository.BaseRepository;
+import com.example.springbootjpa.repository.impl.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
 public class SpringbootJpaApplication {
 
     public static void main(String[] args) {
